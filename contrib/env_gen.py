a6c9073ddb9d2be3c2ae17128787b2c70a19f197
@britodfbr <britodfbr@gmail.com>
2020-10-12 17:01:51 -0300

    Personalização do contrib/env_gen.py

--- contrib/env_gen.py
+++ contrib/env_gen.py
@@ -1,15 +1,18 @@
 # /bin/env python
 # -*- encode: utf-8 -*-
+__author__ = '@britodfbr'
 """
 Django SECRET_KEY generator.
 """
+from string import ascii_lowercase, digits
+from pathlib import Path
 from django.utils.crypto import get_random_string
 
-chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+chars = ascii_lowercase + digits + '!@#$%^&*(-_=+)'
 
-CONFIG_STRING = """
+CONFIG_STRING = f"""
 DEBUG=True
-SECRET_KEY=%s
+SECRET_KEY={get_random_string(50, chars)}
 ALLOWED_HOSTS=127.0.0.1, .localhost
 #DATABASE_URL=postgres://USER:PASSWORD@HOST:PORT/NAME
 #DEFAULT_FROM_EMAIL=
@@ -19,8 +22,8 @@ ALLOWED_HOSTS=127.0.0.1, .localhost
 #EMAIL_USE_TLS=
 #EMAIL_HOST_USER=
 #EMAIL_HOST_PASSWORD=
-""".strip() % get_random_string(50, chars)
+"""
 
 # Writing our configuration file to '.env'
-with open('.env', 'w') as configfile:
-    configfile.write(CONFIG_STRING)
+configfile = Path('.env')
+configfile.write_text(CONFIG_STRING)
